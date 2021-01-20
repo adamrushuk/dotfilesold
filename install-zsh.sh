e747562c8f1df65a0a0989f48001eb919f988885
Adam Rush <adamrushuk@gmail.com>
2021-01-20 10:36:35 +0000

    Changed comments

--- install-zsh.sh
+++ install-zsh.sh
@@ -3,9 +3,9 @@
 #
 # Simple script to install zsh and try to change default shell to it
 #
-echo "Installing zsh..."
+echo -e "\e[34m»»» 🚀 \e[32mInstalling \e[33m'zsh'\e[32m ... \e[39m"
 sudo apt update
 sudo apt install -y zsh
 
-echo "Changing default shell to zsh..."
+echo -e "\e[34m»»» 🚀 \e[32mChanging default shell to \e[33m'zsh'\e[32m ... \e[39m"
 chsh -s /usr/bin/zsh "$USER"
